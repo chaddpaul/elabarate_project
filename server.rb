82b3efe527c46a8bb65a662448b83f53d2177181
Paul Clairmont <pcac25@gmail.com>
2015-01-20 16:04:02 -0500

    adds configure production

--- server.rb
+++ server.rb
@@ -9,6 +9,10 @@ module BarApp
       $redis = Redis.new
     end
 
+    configure :production do
+      $redis = Redis.new({url: ENV['REDISTOGO_URL']})
+    end
+
 # //////////////START Original Get//////////////////
     get('/') do
       redirect to('/home')
